219a9b304bce7286dee3dfa681ef450c56378d49
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2019-02-08 00:05:55 +0300

    Enabled code coverage. Started testing realm service

--- RSS-News-Tests/RealmServiceTests.swift
+++ RSS-News-Tests/RealmServiceTests.swift
@@ -8,15 +8,25 @@
 
 import XCTest
 @testable import RSS_News
+@testable import RealmSwift
 
 class RealmServiceTests: XCTestCase {
     
+    private let realmService: RealmServiceProtocol = RealmService()
+    
     override func setUp() {
-        
+        Realm.Configuration.defaultConfiguration = Realm.Configuration(inMemoryIdentifier: UUID().uuidString)
     }
     
     override func tearDown() {
         
+    }
+    
+    func addToBookmarksTest() {
+        let article = NewsItem(title: "String", iconURL: "String", pubDate: Date(), link: "String", category: "String", source: "String")
+        realmService.addBookmarks([article])
+        let objectsCount = realmService.allBookmarks().count
+        XCTAssertEqual(objectsCount, 1, "Fail")
     }
     
 }
