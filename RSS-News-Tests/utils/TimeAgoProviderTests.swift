718567a3268d54fe55b670dd9134d721ad11cb5a
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2019-02-08 10:39:58 +0300

    Extended time ago provider tests

--- RSS-News-Tests/utils/TimeAgoProviderTests.swift
+++ RSS-News-Tests/utils/TimeAgoProviderTests.swift
@@ -19,31 +19,49 @@ class TimeAgoProviderTests: XCTestCase {
         
     }
     
-    func testHourAgoProvider() {
+    func testHourAgoPositive() {
         let formatter = DateFormatters.with(format: "dd-MM-yyyy HH:mm")
         let hourAgo = Calendar.current.date(byAdding: .hour, value: -1, to: Date())!
         let provider = TimeAgoProvider(date: hourAgo, fullTimeFormatter: formatter)
         let hourAgoComputed = provider.timeAgo()
         let hourAgoResult = L10n.Time.hrAgo(1)
-        XCTAssertEqual(hourAgoComputed, hourAgoResult, "Hour ago is incorrect")
+        XCTAssertEqual(hourAgoComputed, hourAgoResult)
     }
     
-    func testDayAgoProvider() {
+    func testHourAgoNegative() {
+        let formatter = DateFormatters.with(format: "dd-MM-yyyy HH:mm")
+        let hourAgo = Calendar.current.date(byAdding: .hour, value: -1, to: Date())!
+        let provider = TimeAgoProvider(date: hourAgo, fullTimeFormatter: formatter)
+        let hourAgoComputed = provider.timeAgo()
+        let hourAgoResult = L10n.Time.hrAgo(2) // take two ours instead of one
+        XCTAssertNotEqual(hourAgoComputed, hourAgoResult)
+    }
+    
+    func testDayAgoPositive() {
         let formatter = DateFormatters.with(format: "dd-MM-yyyy HH:mm")
         let dayAgo = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
         let provider = TimeAgoProvider(date: dayAgo, fullTimeFormatter: formatter)
         let dayAgoComputed = provider.timeAgo()
         let dayAgoResult = L10n.Time.dayAgo(1)
-        XCTAssertEqual(dayAgoComputed, dayAgoResult, "Day ago is incorrect")
+        XCTAssertEqual(dayAgoComputed, dayAgoResult)
     }
     
-    func testMonthAgoProvider() {
+    func testDayAgoNegative() {
+        let formatter = DateFormatters.with(format: "dd-MM-yyyy HH:mm")
+        let dayAgo = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+        let provider = TimeAgoProvider(date: dayAgo, fullTimeFormatter: formatter)
+        let dayAgoComputed = provider.timeAgo()
+        let dayAgoResult = L10n.Time.dayAgo(2) // take two days instead of one
+        XCTAssertNotEqual(dayAgoComputed, dayAgoResult)
+    }
+    
+    func testMonthAgo() {
         let formatter = DateFormatters.with(format: "dd-MM-yyyy HH:mm")
         let monthAgo = Calendar.current.date(byAdding: .month, value: -1, to: Date())!
         let provider = TimeAgoProvider(date: monthAgo, fullTimeFormatter: formatter)
         let monthAgoComputed = provider.timeAgo()
         let monthAgoResult = formatter.string(from: monthAgo)
-        XCTAssertEqual(monthAgoComputed, monthAgoResult, "Month ago is incorrect")
+        XCTAssertEqual(monthAgoComputed, monthAgoResult)
     }
     
 }
